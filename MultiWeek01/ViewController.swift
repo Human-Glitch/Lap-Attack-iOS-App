4b335bda88e4e8494bb0b8fcb7e96710ad86f79f
Kody Buss <Kody@kodys-mac.local>
2016-10-02 18:20:19 -0500

    Initial Commit

---
+++ MultiWeek01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiWeek01
+//
+//  Created by Kody Buss on 10/2/16.
+//  Copyright © 2016 Kody Buss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
